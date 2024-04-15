af4445ea79ea4c47e8f48e4519875f6b87c9725b
Vladimir Pobedinskiy <vladpob058@gmail.com>
2024-04-15 17:20:15 +0300

    page-transition правки

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 	<AppHeader />
 
 	<main class="main">
-		<transition name="page">
-			<router-view />
-		</transition>
+		<router-view v-slot="{ Component }">
+			<transition name="page" mode="out-in">
+				<component :is="Component" />
+			</transition>
+		</router-view>
 	</main>
 
 	<AppFooter />
@@ -25,11 +27,14 @@ export default {
 <style lang="scss">
 .page-enter-active,
 .page-leave-active {
-	transition: opacity 0.3s ease-in-out;
+	transition:
+		opacity 0.3s ease-in-out,
+		visibility 0.3s ease-in-out;
 }
 
 .page-enter-from,
 .page-leave-to {
 	opacity: 0;
+	visibility: hidden;
 }
 </style>
